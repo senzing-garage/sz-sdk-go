76fefd765e325ce71394a72c75609c47bffd3334
docktermj <michael@dockter.com>
2022-11-09 12:15:26 -0500

    #5 Savepoint

--- g2product/g2product.h
+++ g2product/g2product.h
@@ -3,17 +3,19 @@
 #include <stdlib.h>
 #include "libg2product.h"
 
-typedef void* (*resize_buffer_type)(void*, size_t);
+typedef void *(*resize_buffer_type)(void *, size_t);
 
-struct G2Product_validateLicenseFile_result {
-    char* response;
+struct G2Product_validateLicenseFile_result
+{
+    char *response;
     int returnCode;
 };
 
-struct G2Product_validateLicenseStringBase64_result {
-    char* response;
+struct G2Product_validateLicenseStringBase64_result
+{
+    char *response;
     int returnCode;
 };
 
-struct G2Product_validateLicenseFile_result G2Product_validateLicenseFile_helper(const char* licenseFilePath);
-struct G2Product_validateLicenseStringBase64_result G2Product_validateLicenseStringBase64_helper(const char* licenseString);
+struct G2Product_validateLicenseFile_result G2Product_validateLicenseFile_helper(const char *licenseFilePath);
+struct G2Product_validateLicenseStringBase64_result G2Product_validateLicenseStringBase64_helper(const char *licenseString);
