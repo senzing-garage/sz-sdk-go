9e1967a72789cce83a33896c72f5a22fd9c61ea5
docktermj <michael@dockter.com>
2022-11-15 14:16:36 -0500

    #14 Savepoint

--- g2configmgr/g2configmgr_test.go
+++ g2configmgr/g2configmgr_test.go
@@ -3,6 +3,7 @@ package g2configmgr
 import (
 	"context"
 	"fmt"
+	"log"
 	"strconv"
 	"testing"
 	"time"
@@ -10,6 +11,7 @@ import (
 	truncator "github.com/aquilax/truncate"
 	"github.com/senzing/g2-sdk-go/g2config"
 	"github.com/senzing/go-helpers/g2engineconfigurationjson"
+	"github.com/senzing/go-logging/logger"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,7 +29,8 @@ func getTestObject(ctx context.Context, test *testing.T) G2configmgr {
 	if g2configmgrSingleton == nil {
 		g2configmgrSingleton = &G2configmgrImpl{}
 
-		// g2configmgrSingleton.SetLogLevel(ctx, logger.LevelTrace)
+		g2configmgrSingleton.SetLogLevel(ctx, logger.LevelTrace)
+		log.SetFlags(0)
 
 		moduleName := "Test module name"
 		verboseLogging := 0 // 0 for no Senzing logging; 1 for logging
