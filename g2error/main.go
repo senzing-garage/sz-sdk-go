a686da9fdf22d57142d2f3a9303d7f68d6d841b7
docktermj <michael@dockter.com>
2023-03-20 09:40:35 -0400

    #53 Templates

---
+++ g2error/main.go
@@ -0,0 +1,10 @@
+package g2error
+
+// ----------------------------------------------------------------------------
+// Variables
+// ----------------------------------------------------------------------------
+
+// Message templates for g2engine implementations.
+var G2ErrorTypes = map[int]string{
+	1: "Enter AddRecord(%s, %s, %s, %s).",
+}
