f56d495c98fc219993d1c1f95e7606f30315d4c8
docktermj <michael@dockter.com>
2023-03-20 12:27:03 -0400

    #53 Savepoint

---
+++ g2error/g2error.go
@@ -0,0 +1,110 @@
+package g2error
+
+type G2Error struct {
+	error
+	ForExampleInt    int
+	ForExampleString string
+}
+
+// ----------------------------------------------------------------------------
+// "Category" errors - all based directly on G2Error
+// ----------------------------------------------------------------------------
+
+type G2UnrecoverableInputError struct {
+	G2Error
+}
+
+type G2RetryableError struct {
+	G2Error
+}
+
+type G2BadUserInputError struct {
+	G2Error
+}
+
+// ----------------------------------------------------------------------------
+// Errors based on G2BadUserInputError
+// ----------------------------------------------------------------------------
+
+type G2IncompleteRecordError struct {
+	G2BadUserInputError
+}
+
+type G2MalformedJsonError struct {
+	G2BadUserInputError
+}
+
+type G2MissingConfigurationException struct {
+	G2BadUserInputError
+}
+
+type G2MissingDataSourceError struct {
+	G2BadUserInputError
+}
+
+type G2NotFoundError struct {
+	G2BadUserInputError
+}
+
+type G2UnacceptableJsonKeyValueError struct {
+	G2BadUserInputError
+}
+
+// ----------------------------------------------------------------------------
+// Errors based on G2RetryableError
+// ----------------------------------------------------------------------------
+
+type G2ConfigurationError struct {
+	G2RetryableError
+}
+type G2DatabaseConnectionLostError struct {
+	G2RetryableError
+}
+type G2MessageBufferError struct {
+	G2RetryableError
+}
+type G2RepositoryPurgedError struct {
+	G2RetryableError
+}
+type G2RetryTimeoutExceededError struct {
+	G2RetryableError
+}
+
+// ----------------------------------------------------------------------------
+// Errors based on G2UnrecoverableInputError
+// ----------------------------------------------------------------------------
+
+type G2DatabaseError struct {
+	G2UnrecoverableInputError
+}
+
+type G2ModuleEmptyMessageError struct {
+	G2UnrecoverableInputError
+}
+
+type G2ModuleError struct {
+	G2UnrecoverableInputError
+}
+
+type G2ModuleGenericError struct {
+	G2UnrecoverableInputError
+}
+
+type G2ModuleInvalidXMLError struct {
+	G2UnrecoverableInputError
+}
+
+type G2ModuleLicenseError struct {
+	G2UnrecoverableInputError
+}
+
+type G2ModuleNotInitializedError struct {
+	G2UnrecoverableInputError
+}
+
+type G2ModuleResolveMissingResEntError struct {
+	G2UnrecoverableInputError
+}
+type G2UnhandledError struct {
+	G2UnrecoverableInputError
+}
--- g2error/main.go
+++ g2error/main.go
@@ -1,10 +1,85 @@
 package g2error
 
 // ----------------------------------------------------------------------------
+// Types
+// ----------------------------------------------------------------------------
+
+type G2ErrorTypeIds int
+
+// ----------------------------------------------------------------------------
+// Constants
+// ----------------------------------------------------------------------------
+
+const (
+	G2BadUserInput G2ErrorTypeIds = iota
+	G2Retryable
+	G2Unrecoverable
+	G2BadInput
+	G2Configuration
+	G2DatabaseConnectionLost
+	G2Database
+	G2MalformedJson
+	G2MessageBuffer
+	G2ModuleEmptyMessage
+	G2ModuleGeneric
+	G2ModuleInvalidXML
+	G2ModuleLicense
+	G2ModuleNotInitialized
+	G2ModuleResolveMissingResEnt
+	G2NotFound
+	G2RepositoryPurged
+	G2RetryTimeoutExceeded
+	G2UnacceptableJsonKeyValue
+	G2Unhandled
+)
+
+// ----------------------------------------------------------------------------
 // Variables
 // ----------------------------------------------------------------------------
 
 // Message templates for g2engine implementations.
-var G2ErrorTypes = map[int]type{}{
-	1: RetryableError,
+var G2ErrorTypes = map[int]G2ErrorTypeIds{
+	1:     G2ModuleInvalidXML,
+	2:     G2Unhandled,
+	7:     G2ModuleEmptyMessage,
+	10:    G2RetryTimeoutExceeded,
+	23:    G2UnacceptableJsonKeyValue,
+	24:    G2UnacceptableJsonKeyValue,
+	25:    G2UnacceptableJsonKeyValue,
+	26:    G2UnacceptableJsonKeyValue,
+	27:    G2NotFound,
+	32:    G2UnacceptableJsonKeyValue,
+	33:    G2NotFound,
+	34:    G2Configuration,
+	35:    G2Configuration,
+	36:    G2Configuration,
+	37:    G2NotFound,
+	47:    G2ModuleGeneric,
+	48:    G2ModuleNotInitialized,
+	49:    G2ModuleNotInitialized,
+	50:    G2ModuleNotInitialized,
+	51:    G2UnacceptableJsonKeyValue,
+	53:    G2ModuleNotInitialized,
+	54:    G2RepositoryPurged,
+	61:    G2Configuration,
+	62:    G2Configuration,
+	63:    G2ModuleNotInitialized,
+	64:    G2Configuration,
+	999:   G2ModuleLicense,
+	1001:  G2Database,
+	1007:  G2DatabaseConnectionLost,
+	2089:  G2NotFound,
+	2134:  G2ModuleResolveMissingResEnt,
+	2208:  G2Configuration,
+	7221:  G2Configuration,
+	7426:  G2BadInput,
+	7344:  G2NotFound,
+	9000:  G2ModuleLicense,
+	30020: G2UnacceptableJsonKeyValue,
+	30110: G2MessageBuffer,
+	30111: G2MessageBuffer,
+	30112: G2MessageBuffer,
+	30121: G2MalformedJson,
+	30122: G2MalformedJson,
+	30123: G2MalformedJson,
 }
