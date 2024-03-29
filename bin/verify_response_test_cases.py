6dd5cd9a3ba1f175cc442f40a970eb06c22715cc
docktermj <michael@dockter.com>
2024-03-29 17:35:45 -0400

    #126 Migrate from g2-sdk-go to sz-sdk-go

--- bin/verify_response_test_cases.py
+++ bin/verify_response_test_cases.py
@@ -8,11 +8,8 @@ import json
 import logging
 import os
 
-from test_cases_from_g2_sdk_json_type_definition import TEST_CASES
-
 INPUT_FILE = "bin/response-testcases.json"
 OUTPUT_FILE = "bin/response-testcases-verified.json"
-
 
 # -----------------------------------------------------------------------------
 # --- Main
@@ -46,17 +43,6 @@ for response_class_name, values in response_test_cases.items():
     method_test_cases = values.get("tests", {})
     for _, test_case_json in method_test_cases.items():
         test_case_json_string = json.dumps(test_case_json, sort_keys=True)
-        if test_case_json_string not in aggregation_dict[response_class_name]["tests"]:
-            aggregation_dict[response_class_name]["tests"].append(test_case_json_string)
-        else:
-            duplicates += 1
-
-# Add second source of test cases.
-
-for response_class_name, values in TEST_CASES.items():
-    for _, test_case_json in values.items():
-        test_case_dict = json.loads(test_case_json)
-        test_case_json_string = json.dumps(test_case_dict, sort_keys=True)
         if test_case_json_string not in aggregation_dict[response_class_name]["tests"]:
             aggregation_dict[response_class_name]["tests"].append(test_case_json_string)
         else:
